24f44b856cf1ab9ad732198b9b66a55043730e88
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-11-30 17:44:47 -0500

    adds details to dashboard

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,19 @@ router.get('/', ensureGuest, (req, res) => {
     })
 })
 
-// @desc Dashboard
-// @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
-        const reviews = await Review.find({ user: req.user.id }).lean()
-        res.render('dashboard', {
-            name: req.user.firstName,
-            reviews
-        })
+      const reviews = await Review.find({ user: req.user.id }).lean()
+      res.render('dashboard', {
+        name: req.user.firstName,
+        reviews,
+      })
     } catch (err) {
-        console.error(err)
-    
+      console.error(err)
+      res.render('error/500')
     }
-})
-
-module.exports = router
+  })
+  
+  module.exports = router
